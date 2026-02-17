a2c1b5b5c04abf9173ba6124d1e2b980b128cb6e
Kenneth <krgutgar@gmail.com>
2026-02-17 17:08:33 -0600

    added todo module

---
+++ src/todo.js
@@ -0,0 +1,10 @@
+//Constructor to create a new ToDo
+function toDo(title, description, dueDate, priority) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+}
+
+export { toDo };
